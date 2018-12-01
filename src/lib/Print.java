071c627c2ec4a59b5e227ab86f8be771fd60bbf9
765743262 <765743262@qq.com>
2018-12-01 14:24:31 +0800

    finish Print

--- src/lib/Print.java
+++ src/lib/Print.java
@@ -1,4 +1,27 @@
 package lib;
 
-public class Print {
+import lib.except.MUAExcept;
+import lib.util.ArgUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Print extends Statement {
+
+    public Print(ArrayList<MUAObject> arglist) {
+        super(Type.PRINT, arglist);
+    }
+
+    @Override
+    public void exec(Scope scope) throws MUAExcept {
+        ArgUtil.argCheck(getName(), typelist, arglist);
+        MUAObject obj = arglist.get(0);
+        System.out.println(obj);
+
+
+    }
+
+    private ArrayList<MUAObject.Type> typelist = new ArrayList<MUAObject.Type>(Arrays.asList(
+            MUAObject.Type.ANY
+    ));
 }
