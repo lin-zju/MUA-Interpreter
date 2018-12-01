2290417040a85a3d2cc2a2e65875793d3cb7b1ff
765743262 <765743262@qq.com>
2018-12-01 20:57:03 +0800

    finish all refactoring

--- src/lib/Expr.java
+++ src/lib/Expr.java
@@ -1,26 +1,51 @@
 package lib;
 
-import lib.except.ArgError;
-import lib.util.ArgUtil;
+import lib.except.MUAExcept;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class Expr extends Statement {
-    public Expr(ArrayList<MUAObject> arglist) {
-        super(Type.EXPR, arglist);
+abstract public class Expression {
+
+    public enum SubType {
+        MAKE,
+        ERASE,
+        PRINT,
+        READLIST,
+        THING,
+        ISNAME,
+        REPEAT;
+
+        @Override
+        public String toString() {
+            switch (this) {
+                case MAKE: return "make";
+                case ERASE: return "erase";
+                case PRINT: return "print";
+                case READLIST: return "readlist";
+                case THING: return "thing";
+                case ISNAME: return "isname";
+                case REPEAT: return "repeat";
+            }
+            return "UNKNOWN";
+        }
     }
 
-    @Override
-    public void exec(Scope scope) throws ArgError {
-        ArgUtil.argCheck(getName(), typelist, arglist);
-        MUAObject value = arglist.get(0);
-        System.out.println(value);
-
+    public SubType getSubType() {
+        return subtype;
     }
 
-    private ArrayList<MUAObject.Type> typelist = new ArrayList<MUAObject.Type>(Arrays.asList(
-            MUAObject.Type.ANY
-    ));
+    public String getName() {
+        return subtype.toString();
+    }
+
+
+    protected Expression(SubType subtype, ArrayList<MUAObject> arglist) {
+        this.subtype = subtype;
+        this.arglist = arglist;
+    }
+    abstract public MUAObject eval(Scope scope) throws MUAExcept;
+
+    protected SubType subtype;
+    protected ArrayList<MUAObject> arglist;
 
 }
